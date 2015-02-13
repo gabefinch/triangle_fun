721018cb6c6bc5797a6f3dcf908a7112c12a8fd0
Gabe <gabefinch@gmail.com>
2015-02-13 10:07:31 -0800

    completes isTriangle

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,12 @@
-var triangleBoo = function(side1,side2,side3) {
+var isTriangle = function(side1,side2,side3) {
+  if((side1 >= side2 + side3)||(side2 >= side1 + side3)||(side3 >= side1 + side2)) {
+    return false
+  }
+  else {
+    return true
+  }
+};
+
+var isEquilateral = function(side1,side2,side3) {
   return false
 };
--- spec/specs.js
+++ spec/specs.js
@@ -1,7 +1,23 @@
-describe('triangleBoo', function() {
+describe('isTriangle', function() {
 
-  it('returns false given 3 impossible sides', function () {
-    expect(triangleBoo(1,1,3)).to.equal(false);
+  it('returns false given 3 impossible sides', function() {
+    expect(isTriangle(1,1,3)).to.equal(false);
   })
-  
+
+  it('returns true given 3 true sides', function() {
+    expect(isTriangle(1,1,1)).to.equal(true);
+  })
+
+  it('returns true given 3 barely true sides', function() {
+    expect(isTriangle(1,1,1.99999999999999)).to.equal(true);
+  })
+
+});
+
+describe('isEquilateral', function() {
+
+  it('returns false given 3 non equal sides', function() {
+    expect(isEquilateral(3,4,2)).to.equal(false);
+  })
+
 });
